c6e48d8f6998797d877e40ac471aaabfabf136aa
ilak7 <ilak7@LOCAL>
2022-08-09 13:57:01 +0300

    Добавьте файлы проекта.

---
+++ GrpcMidle/Class1.cs
@@ -0,0 +1,34 @@
+﻿using ProtoBuf.Grpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcMidle
+{
+    public class Class1
+    {
+        [DataContract]
+        public class HelloReply
+        {
+            [DataMember(Order = 1)]
+            public string Message { get; set; }
+        }
+        [DataContract]
+        public class HelloRequest
+        {
+            [DataMember(Order = 1)]
+            public string Name { get; set; }
+        }
+        [ServiceContract]
+        public interface IGreeterService
+        {
+            [OperationContract]
+            Task<HelloReply> SayHelloAsync(HelloRequest request,
+                CallContext context = default);
+        }
+    }
+}
